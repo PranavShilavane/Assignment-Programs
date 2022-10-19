9717edf5539939a5c42228e21f12e34479bea44b
Star <111043921+PranavShilavane@users.noreply.github.com>
2022-10-19 19:36:47 +0530

    Create addTwoNumbers.cpp

---
+++ addTwoNumbers.cpp
@@ -0,0 +1,18 @@
+/*
+
+Program to accept two numbers and display their addition ...
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int a, b;
+
+    cout << "Enter two numbers : ";
+    cin >> a >> b;
+
+    cout << "Addition of two numbers : " << a + b << endl;
+
+    return 0;
+}
